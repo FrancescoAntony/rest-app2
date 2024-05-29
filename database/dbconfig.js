f4c05ddb9c4d8d6b2ec8377ed26737619745decc
FrancescoAntony <francesco31323@gmail.com>
2024-05-29 15:08:45 -0300

    ADS: 01609346 DB

--- database/dbconfig.js
+++ database/dbconfig.js
@@ -1,3 +1,16 @@
+/**
+ * Nome do arquivo: dbconfig.js
+ * Data de criação: 10/05/2024
+ * Autor: Francesco Antony
+ * Matrícula: 01609346
+ *
+ * Descrição:
+ * Este arquivo JavaScript é responsável por mostrar as configurações de acesso
+ * ao meu banco de dados.
+ *
+ * Este script é parte o curso de ADS.
+ */
+
 const sqlite3 = require('sqlite3');
 const sqlite = require('sqlite');
 
