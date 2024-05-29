e6e41bf67ce4f813e12895de5f30619f6a66151b
FrancescoAntony <francesco31323@gmail.com>
2024-05-29 13:51:19 -0300

    ADS: 01609346 DB

---
+++ pages/api/DB.sql
@@ -0,0 +1,19 @@
+/**
+ * Nome do arquivo: exemplo.js
+ * Data de criação: 30/04/2024
+ * Autor: João Silva
+ * Matrícula: 123456
+ *
+ * Descrição:
+ * Este arquivo JavaScript é responsável por implementar as funcionalidades
+ * de interação do usuário com a interface gráfica do módulo de vendas.
+ * Aqui são tratados eventos de cliques, validações de entrada e comunicação
+ * com APIs para envio e recebimento de dados.
+ *
+ * Este script é parte o curso de ADS.
+ */
+
+
+create table cliente {
+
+}
